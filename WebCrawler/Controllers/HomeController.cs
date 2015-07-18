7f78fd1cc3b702cdf5caadbf8d8e6915031c0123
Евгений <Dozer95@bk.ru>
2015-07-19 00:20:54 +0500

    Проект загружен

---
+++ WebCrawler/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using WebCrawler.Models;
+
+namespace WebCrawler.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly StatisticDbEntities entities = new StatisticDbEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var stat = entities.Statistic.ToList();
+            var dates = stat.Select(s => s.UpdatingTime.ToLongDateString()).ToArray();
+            var values = stat.Select(s => s.MembersCount).ToArray();
+
+            var yMin = values.Min()-1;
+            var yMax = values.Max()+1;
+
+            new Chart(800, dates.Length*100)
+                .AddSeries(chartType: "bar",
+                    xValue: dates,
+                    yValues: values)
+                .SetYAxis(null, yMin, yMax)
+                .Save("~/Images/chart.png", "png");
+
+            var model = new StatisticModel
+            {
+                GroupName = entities.GroupInfo.FirstOrDefault()?.GroupName,
+                GroupUrl = entities.GroupInfo.FirstOrDefault()?.GroupUrl,
+                RecordsCount = dates.Length,
+                LastUpdateTime = stat.Max(s => s.UpdatingTime)
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ WebCrawler/Models/StatisticModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebCrawler.Models
+{
+    public class StatisticModel
+    {
+        public string GroupName { get; set; }
+        public int RecordsCount { get; set; }
+        public DateTime LastUpdateTime { get; set; }
+        public string GroupUrl { get; set; }
+    }
+}
