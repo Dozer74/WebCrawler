e079095f8cc5f4f913b0517035274cb43e619f93
Евгений <Dozer95@bk.ru>
2015-07-23 18:13:59 +0500

    Code CleanUp

--- WebCrawler/Scripts/chart.js
+++ WebCrawler/Scripts/chart.js
@@ -1,34 +1,33 @@
-﻿$(document).ready(function () {
+﻿$(document).ready(function() {
     function drawBarChart() {
         $.ajax({
             type: "GET",
             contentType: "application/json; charset=utf-8",
             url: "/Home/BuildChart",
             dataType: "json",
-            success: function (chartData) {
+            success: function(chartData) {
                 var data = new google.visualization.DataTable();
                 data.addColumn("string", "Время");
                 data.addColumn("number", "Подписчиков");
-                $.each(chartData.Records, function (i) {
+                $.each(chartData.Records, function(i) {
                     data.addRow([chartData.Records[i].Date, chartData.Records[i].MembersCount]);
                 });
 
                 var options = {
-                    height: chartData.Records.length*50,
-                    bars: 'horizontal', // Required for Material Bar Charts.
+                    height: chartData.Records.length * 50,
+                    bars: "horizontal", // Required for Material Bar Charts.
                     hAxis: {
-                        format: 'decimal',
+                        format: "decimal",
                         viewWindow: {
                             min: chartData.Min,
                             max: chartData.Max
                         }
                     }
-                }
-
+                };
                 var chart = new google.charts.Bar(document.getElementById("chartdiv"));
                 chart.draw(data, google.charts.Bar.convertOptions(options));
             },
-            error: function (data) {
+            error: function(data) {
                 alert("Error In getting Records");
             }
         });
