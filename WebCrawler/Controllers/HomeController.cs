02ce14162f074970e31990b3c8a1b49d620083b4
Евгений <Dozer95@bk.ru>
2015-07-23 17:34:30 +0500

    График добавлен

--- WebCrawler/Controllers/HomeController.cs
+++ WebCrawler/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Ninject;
+﻿using System.Collections.Generic;
+using System.Globalization;
+using Ninject;
 using System.Linq;
 using System.Web.Mvc;
 using WebCrawler.DAL;
@@ -6,6 +8,12 @@ using WebCrawler.Models;
 
 namespace WebCrawler.Controllers
 {
+    public class TestModel
+    {
+        public string Date;
+        public int MembersCount;
+    }
+
     public class HomeController : Controller
     {
         readonly IKernel kernel = new StandardKernel(new SettingsNinjectModule());
@@ -14,10 +22,10 @@ namespace WebCrawler.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            var dataProvaire = kernel.Get<IDatabaseProvider>();
+            var databaseProvider = kernel.Get<IDatabaseProvider>();
             var groupInfoProvider = kernel.Get<IGroupInfoProvider>();
 
-            var records = dataProvaire.GetAllRecords();
+            var records = databaseProvider.GetAllRecords();
 
             if (!records.Any())
             {
@@ -49,6 +57,27 @@ namespace WebCrawler.Controllers
         private ActionResult EmptyModelView()
         {
             return View("EmptyModel");
+        }
+
+        public JsonResult BuildChart()
+        {
+            var databaseProvider = kernel.Get<IDatabaseProvider>();
+            var records = databaseProvider.GetAllRecords().Reverse();
+
+            var data = records.Select(r => new
+            {
+                Date = r.UpdatingTime.ToString("dd MMMM в HH:mm", CultureInfo.CreateSpecificCulture("ru-RU")),
+                r.MembersCount
+            });//Преобразуем данные для построения графика
+
+            var max = records.Max(r => r.MembersCount);
+            var min = records.Min(r => r.MembersCount);
+
+            var delta = max - min;
+
+            var chartData = new {Records = data, Min = min-delta/3, Max = max+delta/3};
+
+            return Json(chartData, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ WebCrawler/Scripts/MyChart.js
@@ -0,0 +1,42 @@
+﻿$(document).ready(function () {
+    function drawBarChart() {
+        $.ajax({
+            type: "GET",
+            contentType: "application/json; charset=utf-8",
+            url: "/Home/BuildChart",
+            dataType: "json",
+            success: function (chartData) {
+                var data = new google.visualization.DataTable();
+                data.addColumn("string", "Время");
+                data.addColumn("number", "Подписчиков");
+                $.each(chartData.Records, function (i) {
+                    data.addRow([chartData.Records[i].Date, chartData.Records[i].MembersCount]);
+                });
+                var options = {
+                    chart: {
+                        title: "Статистика"
+                    },
+                    legend: { position: "none" },
+                    axes: {
+                        x: {
+                            all: {
+                                range: {
+                                    min: chartData.Min,
+                                    max: chartData.Max
+                                }
+                            }
+                        }
+                    },
+                    bars: "horizontal"
+                };
+                var chart = new google.charts.Bar(document.getElementById("chartdiv"));
+                chart.draw(data, options);
+            },
+            error: function (data) {
+                alert("Error In getting Records");
+            }
+        });
+    }
+
+    drawBarChart();
+});
